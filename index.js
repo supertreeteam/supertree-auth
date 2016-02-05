2a4dd99ad39d6bb10e267561918d79ca2501777f
Lachlan Priest <lachlanpriest@gmail.com>
2016-02-06 00:00:22 +0800

    I don't even know anymore

--- index.js
+++ index.js
@@ -2,5 +2,21 @@
 'use strict';
 
 module.exports = {
-  name: 'supertree-auth'
+  name: 'supertree-auth',
+
+  included: function(app) {
+    this._super.included(app);
+
+    // Nested Addons don't contribute to the Host Application filestructure.
+    // To ensure the initializers from core and checkouts are run, we include
+    // them as dependencies rather than devDependencies, and manually invoke
+    // the Addon included hooks as necessary.
+    this.addons.forEach(function(addon){
+      if (addon.name.substring(0, 11) === "ember-simple-auth") {
+        addon.included.apply(addon, [app]);
+      }
+    });
+
+    app.import('vendor/ember-simple-auth.js');
+  }
 };
